6447ea61149bd728dbadfe49f97c3fa952f71d9e
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2024-03-27 10:07:53 +0200

    distance btn two points

--- function.py
+++ function.py
@@ -2,3 +2,15 @@ def summation(n1, n2):
     sum = n1+n2
     print("sum of 2 numbers: ", sum)
 summation(20,19)
+
+def isPositive(n):
+    result = (n>0)
+    print("isPositive(",  n ,")=", result)
+    return result
+isPositive(-10)
+
+def isEven(n):
+    result = (n%2==0)
+    print("isEven(",  n ,")=", result)
+    return result
+isEven(13)
