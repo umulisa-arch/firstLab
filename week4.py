4843e9236df7219fbcd0a8909f804506902e7fc9
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2024-04-16 13:09:57 +0200

    Week 4 list and dictionary

--- week4.py
+++ week4.py
@@ -9,3 +9,19 @@ print(c)
 print(f.index(2))# to get the position of the element
 d = [0]*10
 print(d)
+
+#create new albitrary 2d list
+import copy
+a = [[2,3,5],[1,4,7]]
+print("our a is: ", a)
+print(len(a[0]))
+
+#shalow copy 
+Christiano = 37
+cr7 = copy.copy(Christiano)
+print(cr7)
+cr7 = 40
+print(cr7)
+print(f"sum of {a[0][1]} and {a[1][-1]} is:  {a[0][1]+a[1][-1]}")
+
+#and deep copy
